9d55ca9647de0f59f89faf5ca4f831fd6a01b351
AireL <tomma2008@hotmail.com>
2016-02-06 16:26:06 +0000

    Added ints generator for testing

--- app/org/scout/controllers/IndexController.scala
+++ app/org/scout/controllers/IndexController.scala
@@ -5,9 +5,13 @@ import play.api.mvc.Controller
 import org.scout.controllers.utilities.ControllerToolkit
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
+import play.api.libs.json.Json
+import scala.util.Random
 
 class IndexController()(implicit override val executionContext: ExecutionContext) extends Controller with ControllerToolkit {
   def index() = GET(
     Ok(org.scout.views.html.layout(org.scout.views.html.navbar(), org.scout.views.html.footer()))
   )
+  
+  def numbers() = JsonGET((1 to 10).toList.map(_ => Random.nextInt()))
 }
